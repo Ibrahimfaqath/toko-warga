6401861a6eb1cb77eb07c6dd9e504a571847e049
Ibrahimfaqath <ibrahimfaqath@gmail.com>
2026-02-07 10:57:13 +0700

    add admin fitur

--- src/db/schema.js
+++ src/db/schema.js
@@ -1,7 +1,6 @@
-// src/db/schema.js
 import { pgTable, serial, varchar, text, integer, numeric, timestamp } from 'drizzle-orm/pg-core';
+import { relations } from 'drizzle-orm';
 
-// 1. Tabel Users (Admin)
 export const users = pgTable('users', {
     id: serial('id').primaryKey(),
     username: varchar('username', { length: 100 }).unique().notNull(),
@@ -9,13 +8,11 @@ export const users = pgTable('users', {
     role: varchar('role', { length: 20 }).default('customer'),
 });
 
-// 2. Tabel categories
 export const categories = pgTable('categories', {
     id: serial('id').primaryKey(),
     name: varchar('name', { length: 100 }).notNull(),
 });
 
-// 3. Tabel products
 export const products = pgTable('products', {
     id: serial('id').primaryKey(),
     name: varchar('name', { length: 256 }).notNull(),
@@ -26,17 +23,15 @@ export const products = pgTable('products', {
     categoryId: integer('category_id').references(() => categories.id),
 });
 
-// 4. Tabel orders
 export const orders = pgTable('orders', {
     id: serial('id').primaryKey(),
     customerName: varchar('customer_name', { length: 256 }).notNull(),
     address: text('address').notNull(),
-    totalAmount: numeric('total_amount', { precision: 12, scale:2 }).notNull(),
-    status: varchar('status', { length: 20 }).default('pending'), // pending, paid
+    totalAmount: numeric('total_amount', { precision: 12, scale: 2 }).notNull(),
+    status: varchar('status', { length: 20 }).default('pending'),
     createdAt: timestamp('created_at').defaultNow(),
 });
 
-// 5. Tabel Order Items (Detail Belanjaan)
 export const orderItems = pgTable('order_items', {
     id: serial('id').primaryKey(),
     orderId: integer('order_id').references(() => orders.id).notNull(),
@@ -44,3 +39,13 @@ export const orderItems = pgTable('order_items', {
     quantity: integer('quantity').notNull(),
     priceAtTime: numeric('price_at_time', { precision: 12, scale: 2 }).notNull(),
 });
+
+// RELASI (Ini yang bikin data muncul di Admin)
+export const ordersRelations = relations(orders, ({ many }) => ({
+    items: many(orderItems),
+}));
+
+export const orderItemsRelations = relations(orderItems, ({ one }) => ({
+    order: one(orders, { fields: [orderItems.orderId], references: [orders.id] }),
+    product: one(products, { fields: [orderItems.productId], references: [products.id] }),
+}));
